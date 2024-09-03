a04d79048b6a48578b7576652ec113cedfdd54d2
Hao Wu <echowuhao@gmail.com>
2024-09-03 11:21:32 +0800

    Update Makefile, views.py, package-lock.json, package.json, audio_spliter.vue, add AudioRecorder component, and update api.js for audio file handling and version bump to 0.1.29

--- web/src/views/base/audio_spliter.vue
+++ web/src/views/base/audio_spliter.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="h-screen flex flex-col">
     <header class="m-4 p-4">
-      <h1 class="font-bold mb-4">Audio File Splitter</h1>
+      <h1 class="font-bold mb-4">Add Audio</h1>
       <div class="mt-20 ml-20">
-      <FileUpload @file-uploaded="refreshAudioFiles" />
-</div>
+        <n-tabs>
+          <n-tab-pane name="record" tab="Record Audio">
+            <AudioRecorder @recording-uploaded="refreshAudioFiles" />
+          </n-tab-pane>
+          <n-tab-pane name="upload" tab="Upload File">
+            <FileUpload @file-uploaded="refreshAudioFiles" />
+          </n-tab-pane>
+        </n-tabs>
+      </div>
     </header>
     <main class="flex-grow overflow-hidden">
       <div ref="audioListContainer" class="h-full overflow-y-auto p-4">
@@ -18,6 +25,7 @@
 import { ref, onMounted } from 'vue';
 import FileUpload from './components/FileUpload.vue';
 import AudioFileList from './components/AudioFileList.vue';
+import AudioRecorder from './components/AudioRecorder.vue';
 import api from './services/api';
 
 const audioFiles = ref([]);
