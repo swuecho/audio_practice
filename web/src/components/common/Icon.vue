a7ecc5afd6ce0824041a084fe0b2fe514fe258c5
Hao Wu <echowuhao@gmail.com>
2024-09-04 20:48:03 +0800

    Refactor Icon component and remove base and upload demo views

--- web/src/components/common/Icon.vue
+++ web/src/components/common/Icon.vue
@@ -14,6 +14,7 @@ export default defineComponent({
 
   computed: {
     icon() {
+      //@ts-ignore
       return defineAsyncComponent(() => import(`../../assets/icons/editor/${this.name}.svg`));
     },
   },
